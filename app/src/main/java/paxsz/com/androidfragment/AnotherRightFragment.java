922c50acba39ea6f17686f2c8685e682c6c08000
TangJian(唐健/深圳) <tangjian@paxsz.local>
2017-12-29 14:39:43 +0800

    动态Fragment

---
+++ app/src/main/java/paxsz/com/androidfragment/AnotherRightFragment.java
@@ -0,0 +1,22 @@
+package paxsz.com.androidfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/*
+ *  创建者:   tangjian
+ *  创建时间:  2017/12/29 11:27
+ *  描述：    TODO
+ */
+public class AnotherRightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment, container, false);
+        return view;
+    }
+}
