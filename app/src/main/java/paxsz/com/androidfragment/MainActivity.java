fc99ed8f6b3ae418195be02c809fd8a32f693410
TangJian(唐健/深圳) <tangjian@paxsz.local>
2018-01-02 18:08:49 +0800

    使用限定符

--- app/src/main/java/paxsz/com/androidfragment/MainActivity.java
+++ app/src/main/java/paxsz/com/androidfragment/MainActivity.java
@@ -1,9 +1,6 @@
 package paxsz.com.androidfragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +13,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
-        replaceFragment(new RightFragment());
+//        replaceFragment(new RightFragment());
     }
 
 
@@ -24,19 +21,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                replaceFragment(new AnotherRightFragment());
+//                replaceFragment(new AnotherRightFragment());
                 break;
             default:
                 break;
         }
     }
 
-    private void replaceFragment(Fragment fragment) {
+   /* private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.right_layout, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
-    }
+    }*/
 
 }
